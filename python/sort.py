e0ac16153d5c795673f5bdaf2457da2476a4046a
tuxl <tuxl@yunhorn.com>
2017-01-18 16:59:10 +0800

    希尔排序

--- python/sort.py
+++ python/sort.py
@@ -32,12 +32,31 @@ def bubblesort(data):
             i += 1
         index += 1
 
+def _shellinsertsort(data, start, datalen, step):
+    index = 0
+    while index < datalen-1:
+        i = index
+        while i >= 0 :
+            if data[start+(i*step)] > data[start+((i+1)*step)]:
+                temp = data[start+((i+1)*step)]
+                data[start + ((i + 1) * step)] = data[start+(i*step)]
+                data[start + (i * step)] = temp
+            i -= 1
+        index += 1
+
 def shellsort(data):
-    pass
+    #希尔排序
+    originallen = len(data)
+    step = int(originallen/2)
+    while step > 0:
+        times = 0
+        while times < step:
+            _shellinsertsort(data, times, int(((originallen-(times+1))/step))+1, step)
+            times += 1
+        step = int(step/2)
 
 data = [random.randint(0,100) for i in range(10)]
 # data = [49, 38, 65, 97, 76, 13, 27, 49, 55, 4]
 print('排序前 --- ', data)
-bubblesort(data)
+shellsort(data)
 print('排序后 --- ', data)
-
